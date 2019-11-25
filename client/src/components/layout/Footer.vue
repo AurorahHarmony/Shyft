0b595d81296b9c562e6cb9094f7efd5c733fe957
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-11-26 00:56:29 +1300

    Add basic footer content

--- client/src/components/layout/Footer.vue
+++ client/src/components/layout/Footer.vue
@@ -1,6 +1,18 @@
 <template>
   <footer>
-    <hr />This is the footer
+    <div class="content has-text-centered">
+      <p>
+        <strong>Shyft</strong> by
+        <a href="https://github.com/AurorahHarmony">Aurorah Harmony</a>. The source code is licensed
+        <a
+          href="https://www.gnu.org/licenses/gpl-3.0.en.html"
+        >GNU General Public License v3.0</a>. The website content
+        is licensed
+        <a
+          href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+        >CC BY NC ND 4.0</a>.
+      </p>
+    </div>
   </footer>
 </template>
 <script>
