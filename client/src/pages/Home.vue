9c4c1914691e8d3278329548e1d41833a918ea7e
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-11-25 14:08:58 +1300

    Create Home component

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="home">
+    this is the home page!~
+    <Button class="button">Blep!</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
